6b71ae2576202f87ad742c39afa10c4406541535
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-01-28 12:49:53 +0000

    Update spatial_kde test

--- tests/test_kde.py
+++ tests/test_kde.py
@@ -1,22 +1,26 @@
 from pathlib import Path
 
 import geopandas as gpd
-import numpy as np
+import pytest
+import rasterio
 
 from spatial_kde import spatial_kernel_density
 
 
-def test_spatial_kernel_density(data_dir, tmp_path):
+def test_spatial_kernel_density_no_weight(data_dir, tmp_path):
     gdf = gpd.read_file(str(data_dir / "points_epsg_32630.gpkg"))
     out_file = str(tmp_path / "out.tif")
 
-    out_arr = spatial_kernel_density(
+    spatial_kernel_density(
         points=gdf,
         radius=100,
-        output_pixel_size=1,
-        weight_col="weight",
-        output_raster=out_file,
+        output_pixel_size=2,
+        output_path=out_file,
+        scaled=False,
     )
 
-    assert isinstance(out_arr, np.ndarray)
     assert Path(out_file).exists()
+
+    with rasterio.open(out_file) as src:
+        out_arr = src.read(1)
+        assert max(out_arr.flatten()) == pytest.approx(1.58, abs=1e-2)
