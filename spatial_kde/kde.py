1148d5e0933882164490c26b3712f85211f21de6
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-01-28 12:47:24 +0000

    Add simple brute force implementation

--- spatial_kde/kde.py
+++ spatial_kde/kde.py
@@ -1,35 +1,24 @@
-from typing import Optional, Tuple
+from typing import Optional
 
 import geopandas as gpd
 import numpy as np
-import pandas as pd
-from scipy.spatial.kdtree import cKDTree
+import rasterio
+from rasterio.crs import CRS
 from shapely.geometry import Point
 
 from spatial_kde.kernels import quartic
-
-
-def _kde_at(
-    row: pd.Series,
-    radius: float,
-    weight_col: Optional[str] = None,
-) -> float:
-    """Helper function used by pandas.DataFrame.apply() to calculate the KDE"""
-    return quartic(
-        distances=np.array(row.distances),
-        radius=radius,
-        weights=np.array(row[weight_col]) if weight_col else None,
-    )
+from spatial_kde.utils import Bounds
 
 
 def spatial_kernel_density(
     points: gpd.GeoDataFrame,
     radius: float,
+    output_path: str,
     output_pixel_size: float,
+    output_driver: str = "GTiff",
     weight_col: Optional[str] = None,
-    extent: Optional[Tuple[float, float, float, float]] = None,
-    output_raster: str = None,
-) -> np.ndarray:
+    scaled: bool = False,
+) -> None:
     """Calculate Kernel Density / heatmap from ``points``
 
     .. note:: Distance calculations are planar so care should be taken with data
@@ -42,9 +31,14 @@ def spatial_kernel_density(
     radius : float
         Radius of KDE, same units as the coordinate reference system of ``points``
         Sometimes referred to as search radius or bandwidth
+    output_path : str
+        Path to write output raster to
     output_pixel_size : float
         Output cell/pixel size of the created array. Same units as the coordinate
         reference system of ``points``
+    output_driver : str
+        Output format (driver) used to create image. See also
+        https://rasterio.readthedocs.io/en/latest/api/rasterio.drivers.html
     weight_col : Optional[str], optional
         A column in ``points`` to weight the kernel density by, any points that
         are NaN in this field will not contribute to the KDE.
@@ -62,63 +56,68 @@ def spatial_kernel_density(
         raise ValueError(f"`{weight_col}` column not found in `points` GeoDataFrame")
 
     if weight_col:
-        # TODO need to copy `points` here?
         points = points.dropna(subset=[weight_col])
 
-    # setup the extent from given or from the total bounds of the input data
-    if extent is None:
-        minx, miny, maxx, maxy = points.total_bounds
-    else:
-        minx, miny, maxx, maxy = extent
-
-    # create mesh grid array of the correct extent and pixel size
-    x_grid = np.arange(minx - radius, maxx + radius, output_pixel_size)
-    y_grid = np.arange(miny - radius, maxy + radius, output_pixel_size)
-    x_mesh, y_mesh = np.meshgrid(x_grid, y_grid)
+    # Get the bounding box extent for the new raster
+    bounds = Bounds.from_gdf(gdf=points, radius=radius)
 
     # Create x/y coordinate pairs for neighbour calculations on the kd-tree
-    xy = np.column_stack((x_mesh.flatten(), y_mesh.flatten()))
-
-    # Create a KDTree for all the points so we can more efficiently do a lookup
-    # for nearby points when calculating the KDE
-    kdt = cKDTree(
-        np.column_stack(
-            (
-                points.geometry.apply(lambda g: g.centroid.x).to_numpy(),
-                points.geometry.apply(lambda g: g.centroid.y).to_numpy(),
-            )
-        )
+    x_mesh, y_mesh = np.meshgrid(
+        bounds.x_coords(output_pixel_size),
+        bounds.y_coords(output_pixel_size),
     )
 
-    # Find all the points on the grid that have neighbours within the search
-    # radius, these are the non-zero points of the KDE surface
-    kde_pnts = pd.DataFrame(kdt.query_ball_point(xy, r=radius), columns=["idxs"])
-    kde_pnts["num"] = kde_pnts.idxs.apply(len)
+    # create the coordinate grid of the pixel centre points
+    xc = x_mesh + (output_pixel_size / 2)
+    yc = y_mesh + (output_pixel_size / 2)
 
-    # Filter out points that have no neighbours within the search radius
-    # and therefore their KDE value will be 0
-    kde_pnts = kde_pnts.query("num > 0").drop(columns=["num"]).reset_index()
+    # Slow implementation iterating over every pixel / point
+    ndv = -9999
+    Z = ndv + np.zeros_like(xc)
 
-    # Store the centroid (i.e. the KDE raster pixel centre), and calculate the
-    # distances all neighbours that are within the radius
-    # TODO geodesic distance support
-    kde_pnts["centroid"] = kde_pnts["index"].apply(lambda i: Point(xy[i]))
-    kde_pnts["distances"] = kde_pnts.apply(
-        lambda r: [r.centroid.distance(Point(xy[i])) for i in r.idxs], axis=1
-    )
+    for row in range(xc.shape[0]):
+        for col in range(xc.shape[1]):
 
-    # add the weights array
-    if weight_col is not None:
-        kde_pnts[weight_col] = kde_pnts.apply(
-            lambda r: [points.at[i, weight_col] for i in r.idxs],
-            axis=1,
-        )
+            distances = []
+            weights = []
+            for pnt in points.itertuples():
+                # Euclidean distance (i.e. planar)
+                # TODO add geodesic distance support
+                centre = Point((xc[row][col], yc[row][col]))
+                dist = centre.distance(pnt.geometry)
 
-    # Calculate the KDE value for each point
-    kde_pnts["kde"] = kde_pnts.apply(lambda r: _kde_at(r, radius, weight_col), axis=1)
+                if dist <= radius:
+                    distances.append(dist)
 
-    # TODO
-    # convert the KDE values into a 2D array
-    # Optionally write to raster? Or separate function?
+                if weight_col:
+                    weights.append(pnt[weight_col])
 
-    return np.array()
+            if distances:
+                Z[row, col] = quartic(
+                    distances=distances,
+                    radius=radius,
+                    weights=weights if weight_col else None,
+                    scaled=scaled,
+                )
+
+    # create the output raster
+    with rasterio.open(
+        output_path,
+        "w",
+        driver=output_driver,
+        width=Z.shape[1],
+        height=Z.shape[0],
+        count=1,
+        dtype=Z.dtype,
+        crs=CRS.from_user_input(points.crs),
+        transform=rasterio.transform.from_bounds(
+            west=bounds.min_x,
+            south=bounds.min_y,
+            east=bounds.max_x,
+            north=bounds.max_y,
+            width=Z.shape[1],
+            height=Z.shape[0],
+        ),
+        nodata=ndv,
+    ) as dst:
+        dst.write(Z, 1)
