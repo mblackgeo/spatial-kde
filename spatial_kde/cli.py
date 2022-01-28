0488186fa8f7df3ba6c3f0c16f2b63b93246921e
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-01-28 14:21:17 +0000

    Implement CLI wrapper for the main function

--- spatial_kde/cli.py
+++ spatial_kde/cli.py
@@ -1,4 +1,9 @@
+from typing import Optional
+
+import geopandas as gpd
 import typer
+
+from spatial_kde import spatial_kernel_density
 
 app = typer.Typer()
 
@@ -7,6 +12,26 @@ app = typer.Typer()
 def kde_from_vector(
     vector: str = typer.Argument(..., help="Path to input vector file"),
     output: str = typer.Argument(..., help="Output path for created raster"),
+    radius: float = typer.Option(
+        default=1,
+        help="Radius/Bandwith for the KDE. Same units as the CRS of `vector`.",
+    ),
+    output_pixel_size: float = typer.Option(
+        default=1,
+        help="Output pixel size (resolution). Same units as the CRS of `vector`.",
+    ),
+    output_driver: str = typer.Option(
+        default="GTiff",
+        help="Output driver (file format) used by rasterio (Default = GeoTiff).",
+    ),
+    weight_field: Optional[str] = typer.Option(
+        default=None,
+        help="Optional field in `vector` containing weights of each point.",
+    ),
+    scaled: bool = typer.Option(
+        default=False,
+        help="Set to True to scale the KDE values, leave false to use raw values.",
+    ),
 ):
     """
     Create a Spatial Kernel Density / Heatmap raster from an input vector.
@@ -14,4 +39,12 @@ def kde_from_vector(
     The input vector file must be readable by GeoPandas and contain Point type
     geometry (for non-point geometries the centroid will be used for the KDE).
     """
-    pass
+    spatial_kernel_density(
+        points=gpd.read_file(vector),
+        radius=radius,
+        output_path=output,
+        output_pixel_size=output_pixel_size,
+        output_driver=output_driver,
+        weight_col=weight_field,
+        scaled=scaled,
+    )
